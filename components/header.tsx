9e240108c54b791913d9d8641dd8655904c2fc26
cristobalobos <cristobal.lobost@gmail.com>
2025-04-22 11:32:35 -0400

    Change Name

--- components/header.tsx
+++ components/header.tsx
@@ -12,8 +12,7 @@ export default function Header() {
       <div className="container mx-auto px-4">
         <div className="flex items-center justify-between h-16">
           <Link href="/" className="font-bold text-xl text-green-800">
-            Lobos Patagonia Travel
-          </Link>
+            Lobos Transfer Los Lagos          </Link>
 
           <nav className="hidden md:flex items-center space-x-6">
             <Link href="/" className="text-stone-700 hover:text-green-700 transition-colors">
